6751fa222b2beba89dde2d12992d908c6e63be15
jasmin-alimanovic <62791380+jasmin-alimanovic@users.noreply.github.com>
2021-02-04 12:08:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'jasko.alimanovic@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'PI3141592'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jasko.alimanovic@gmail.com']
     MAIL_DEFAULT_SENDER = MAIL_USERNAME
 
